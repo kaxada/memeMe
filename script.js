4debb40a32698cf51b9d4c52cf5c6120e20dc274
EnockKasaadha <enocksdha@gmail.com>
2020-04-05 09:37:13 +0300

    fixed bug: changed DOM bottom element ref and added bottom text function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let topTextInput,
     canvas,
     ctx;
 
-function generateMeme(img, topText) {
+function generateMeme(img, topText, bottomText) {
     canvas.width = img.width; // set canvas width to image width
     canvas.height = img.height; // set canvas height to image height
 
@@ -23,13 +23,20 @@ function generateMeme(img, topText) {
 
     // draw the top text
     ctx.textBaseline = 'top';
-    ctx.fillText(topText, canvas.width / 2, 0, canvas.width);
+    ctx.fillText(topText, canvas.width / 2, 0,
+        canvas.width);
+    ctx.strokeText(canvas.width / 2, 0, canvas.width)
+
+    // draw the bottom text
+    ctx.textBaseline = 'bottom';
+    ctx.fillText(bottomText, canvas.width / 2, canvas.height, canvas.width);
+    ctx.strokeText(canvas.width / 2, canvas.height, canvas.width)
 }
 
 function init() {
     // grab the HTML tags 
     topTextInput = document.getElementById('top-text');
-    bottomTextInput = document.getElementById('bottom-element');
+    bottomTextInput = document.getElementById('bottom-text');
     imageInput = document.getElementById('image-input');
     generateBtn = document.getElementById('generate-btn');
     canvas = document.getElementById('meme-canvas');
@@ -44,7 +51,7 @@ function init() {
         reader.onload = function() {
             let img = new Image; // make a new image with Image object
             img.src = reader.result; // choose image source with src method
-            generateMeme(img, topTextInput.value); // the last two parameters get the values of the arguments
+            generateMeme(img, topTextInput.value, bottomTextInput.value); // the last two parameters get the values of the arguments
         };
         reader.readAsDataURL(imageInput.files[0]); // select image to draw on canvas
 
